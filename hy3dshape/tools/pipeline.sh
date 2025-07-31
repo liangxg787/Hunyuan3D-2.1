689672a591ec42a224ff08b199309980a2680661
Ngene <hplxg@hotmail.com>
2025-07-31 17:00:03 +0100

    fix: AttributeError: Calling operator "bpy.ops.import_scene.obj" error, could not be found

--- hy3dshape/tools/pipeline.sh
+++ hy3dshape/tools/pipeline.sh
@@ -7,6 +7,6 @@ export BLENDER_PATH=/nobackup/babbage/users/xl01339/blender-4.0.2-linux-x64/blen
 export INPUT_FILE=/nobackup/babbage/users/xl01339/03001627_10/1a38407b3036795d19fb4103277a6b93/export INPUT_FILE=/nobackup/babbage/users/xl01339/03001627_10/1a38407b3036795d19fb4103277a6b93/model.obj
 export NAME=model
 
-$BLENDER_PATH -b -P render/render.py -- --object ${INPUT_FILE} --output_folder $OUTPUT_FOLDER/$NAME/render_cond --geo_mode --resolution 512
+#$BLENDER_PATH -b -P render/render.py -- --object ${INPUT_FILE} --output_folder $OUTPUT_FOLDER/$NAME/render_cond --geo_mode --resolution 512
 # $BLENDER_PATH -b -P render/render.py -- --object ${INPUT_FILE} --output_folder $OUTPUT_FOLDER/$NAME/render_tex --resolution 512
 python3 watertight/watertight_and_sample.py --input_obj $OUTPUT_FOLDER/$NAME/render_cond/mesh.ply --output_prefix $OUTPUT_FOLDER/$NAME/geo_data/$NAME
